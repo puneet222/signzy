462ffe8b2220384dca6cf7b2a18bad87fe9578ae
Puneet Aggarwal <9988182547p@gmail.com>
2016-10-31 21:48:24 +0530

    refined searching

--- getResult.php
+++ getResult.php
@@ -2,18 +2,26 @@
 include('dbcon.php') ;
 $str = $_GET['q'] ;
 // echo "ashjbsa" ;
-$srch = "'".$str."%'" ;
+$arr = explode(" " , $str) ;
+$size = count($arr) ;
+// echo "size is : ".$size ;
+
+$srch = "'".$arr[0]."%'" ;
 
 // starting from the first name
+$pass = array() ;
+if ($size < 2)
+{
 
-$query = "SELECT * FROM main WHERE first like ".$srch.";" ;
+	// echo "I'm in if statement" ;
+	$query = "SELECT * FROM main WHERE first like ".$srch.";" ;
  	// echo $query ;
 	$result = $conn->query($query);
 	$count1 = mysqli_num_rows($result) ;  // get the count
 
 	// echo "number of rows are for 1 : ".$count1 ; 
 	// echo $result;
-	$pass = array() ;
+	
 		if($count1 >= 5){                                        // got all the first name
 			 while($row = $result->fetch_assoc()) {
 	    	// echo "im loop" ;
@@ -66,7 +74,7 @@ $query = "SELECT * FROM main WHERE first like ".$srch.";" ;
 
 
 				// pattern in the first name or last name 
-				$srch2 = "'%".$str."%'" ;
+				$srch2 = "'%".$arr[0]."%'" ;
 				$query = "SELECT * FROM main WHERE first like ".$srch2.";" ;
 			 	// echo $query ;
 				$result = $conn->query($query);
@@ -105,6 +113,69 @@ $query = "SELECT * FROM main WHERE first like ".$srch.";" ;
 		   		}
 			} 
 		}
+} // end of main if statement
+else
+{
+	// echo "in else statement" ;
+	$sr1 = "'".$arr[0]."%'" ;
+	$sr2 = "'".$arr[1]."%'" ;
+// echo "first :".$arr[0]."  "."second : ".$arr[1] ;
+	$query = "SELECT * FROM main WHERE first like ".$sr1." AND last like ".$sr2.";"  ;
+	// echo $query ;
+	$result = $conn->query($query);
+
+	while($row = $result->fetch_assoc()){
+		$pass[] = $row ;
+	}
+
+	$count1 = mysqli_num_rows($result) ;
+
+	if($count1 < 5)
+	{
+		$lim1 = 5 - $count1 ;
+		$sr1 = "'%".$arr[0]."%'" ;
+		$sr2 = "'".$arr[1]."%'" ;
+	// echo "first :".$arr[0]."  "."second : ".$arr[1] ;
+		$query = "SELECT * FROM main WHERE first like ".$sr1." AND last like ".$sr2.";"  ;
+		// echo $query ;
+		$result = $conn->query($query);
+
+		while($row = $result->fetch_assoc()){
+			$pass[] = $row ;
+		}
+
+		$count2 = mysqli_num_rows($result) ;
+
+		$sr1 = "'".$arr[0]."%'" ;
+		$sr2 = "'%".$arr[1]."%'" ;
+	// echo "first :".$arr[0]."  "."second : ".$arr[1] ;
+		$query = "SELECT * FROM main WHERE first like ".$sr1." AND last like ".$sr2.";"  ;
+		// echo $query ;
+		$result = $conn->query($query);
+
+		while($row = $result->fetch_assoc()){
+			$pass[] = $row ;
+		}
+
+		$count3 = mysqli_num_rows($result) ;
+
+		if($count2+$count3 < $lim1)
+		{
+			$lim2 = $lim1 - $count2 ;
+			$sr1 = "'%".$arr[0]."%'" ;
+			$sr2 = "'%".$arr[1]."%'" ;
+			// echo "first :".$arr[0]."  "."second : ".$arr[1] ;
+			$query = "SELECT * FROM main WHERE first like ".$sr1." AND last like ".$sr2.";"  ;
+			// echo $query ;
+			$result = $conn->query($query);
+
+			while($row = $result->fetch_assoc()){
+				$pass[] = $row ;
+			}
+
+		}
+	}
+}
 		
 
 $names = json_encode($pass) ;
